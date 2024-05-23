68a0448610f5725b415555fd50ce8debc7ea2ba8
jordy-ricardo25 <jordy.ricardo25@outlook.com>
2024-05-23 06:07:03 -0500

    Add support for .NET Framework 4.8 n .NET Core 3.1

---
+++ DN.Testing/Views/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Windows.Controls;
+using System.Windows;
+using MSB.UI.Controls;
+
+namespace DN.Testing.Views
+{
+    /// <summary>
+    /// Interaction logic for MainPage.xaml
+    /// </summary>
+    public partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnShowTeachingTipButtonClick(object sender, RoutedEventArgs e)
+        {
+            TT.Title = "Hello, World!";
+            TT.Description = "Welcome to .NET";
+            TT.PreferredPlacement = TeachingTipPlacementMode.Top;
+            TT.VerticalOffset = 32;
+            TT.IsLightDismissEnabled = false;
+            TT.IsOpen = true;
+        }
+
+        private void OnShowMessageDialogButtonClick(object sender, RoutedEventArgs e)
+        {
+            new MessageDialog()
+            {
+                Title = "Hello, World!",
+                Message = "Welcome to .NET"
+            }.Show();
+        }
+    }
+}
